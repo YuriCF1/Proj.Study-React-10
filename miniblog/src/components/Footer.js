14543022ced47edf309e9ab81c19b6c778e8a121
Yuri <yurifdev@gmail.com>
2023-01-23 20:36:05 -0300

    Creating components of Navbar and Footer

---
+++ miniblog/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+  return (
+    <footer>
+        <h3>Escreva o que tens de interesse</h3>
+        <p>Mini Blog &copy; 2023</p>
+    </footer>
+  );
+}
+
+export default Footer;
