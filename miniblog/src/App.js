20d5c7353dbe590759c9f647d5ef8519d48508a8
Yuri <yurifdev@gmail.com>
2023-01-24 12:22:23 -0300

    Creating routes to register and login

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -6,19 +6,23 @@ import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <NavBar />
-          <div className="container"> 
-            <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/about" element={<About />}></Route>
-            </Routes>
-          </div>
-          <Footer />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
