21fbdb26adcce4cfaca9b94bb1d51577f62e9f9e
Yuri <yurifdev@gmail.com>
2023-01-24 15:43:10 -0300

    Creating functions to the form

--- miniblog/src/pages/Register/Register.js
+++ miniblog/src/pages/Register/Register.js
@@ -5,19 +5,48 @@ import { useState, useEffect } from "react";
 import styles from "./Register.module.css";
 
 const Register = () => {
+  const [displayname, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    const user = {
+      displayname,
+      email,
+      password,
+    };
+
+    if (password !== confirmPassword) {
+      setError("As senhas precisam ser iguais. Por vafor, verifique novamente");
+      return console.error(error);
+    }
+
+    console.log(user);
+  };
+
   return (
-    <div>
+    <div className={styles.register}>
       <h1>Cadastre-se para postar</h1>
       <p>Crie seu usuário e compartilhe suas histórias</p>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <label htmlFor="">
           <span>Nome:</span>
           <input
             type="text"
             name="displayname"
-            id=""
             required
             placeholder="Nome do usuário"
+            autoComplete="username"
+            value={displayname}
+            onChange={(e) => {
+              setDisplayName(e.target.value);
+            }}
           />
         </label>
         <label htmlFor="">
@@ -25,9 +54,13 @@ const Register = () => {
           <input
             type="email"
             name="email"
-            id=""
             required
             placeholder="E-mail do usuário"
+            autoComplete="username email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
           />
         </label>
         <label htmlFor="">
@@ -35,21 +68,30 @@ const Register = () => {
           <input
             type="password"
             name="password"
-            id=""
             required
             placeholder="Crie aqui sua senha"
-          />
+            autoComplete="username new-password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            />
         </label>
         <label htmlFor="">
           <span>Confirmação de senha:</span>
           <input
             type="password"
             name="confirmPassword"
-            id=""
             required
             placeholder="Confirme aqui sua senha"
+            autoComplete="username new-password"
+            value={confirmPassword}
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+            }}
           />
         </label>
+        {error && <p className="error">{error}</p> }
         <button className="btn">Cadastrar</button>
       </form>
     </div>
