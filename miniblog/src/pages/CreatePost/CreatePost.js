5e07a3114408edccf8e0700ed3da04b953bca505
Yuri <yurifdev@gmail.com>
2023-01-27 14:03:06 -0300

    Creating folders of new post page and dashboard page

---
+++ miniblog/src/pages/CreatePost/CreatePost.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./CreatePost.module.css"
+
+const CreatePost = () => {
+  return (
+    <div><h2>CreatePost</h2></div>
+  )
+}
+
+export default CreatePost
