db35a10e218cbaa3a5a3915b301e2929b832bb70
Yuri <yurifdev@gmail.com>
2023-01-27 11:45:02 -0300

    Getting the user from authentication hood and setting into context

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth"; //Mapeia se a autentificação do usuário foi bem sucedida
+
+// Hooks
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
 
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
@@ -13,9 +18,24 @@ import Login from "./pages/Login/Login";
 import { AuthProvider } from "./context/AuthContext";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div className="App">
-      <AuthProvider>
+      <AuthProvider value={{ user }}>
         <BrowserRouter>
           <NavBar />
           <div className="container">
