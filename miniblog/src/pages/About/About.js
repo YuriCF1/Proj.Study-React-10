cde45b0d13272556d441096931e3ac5ac026ea41
Yuri <yurifdev@gmail.com>
2023-01-28 10:24:22 -0300

    Creating text About page

--- miniblog/src/pages/About/About.js
+++ miniblog/src/pages/About/About.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 //Css
-import styles from "./About.module.css"
-
+import styles from "./About.module.css";
 
 const About = () => {
   return (
-    <div>
-        <h1>About</h1>
+    <div className={styles.about}>
+      <h1>
+        Sobre o Min<span>Blog</span>
+      </h1>
+      <p>
+        Este projeto é feito para evolução nas prática do React no Frontend,
+        utilizando também firebase, no backend. Com criação de usuários, posts,
+        login, logout e outras features presentes na maioria das redes sociais
+      </p>
+      <span>Teste o app agora mesmo!</span>
+      <Link to={"/post/create"} >
+         <button className="btn">Criar Post</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
