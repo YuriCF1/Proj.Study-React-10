fdbeaa27f861ab579cd6e7eb9db763ae6492e6d6
Yuri <yurifdev@gmail.com>
2023-02-02 10:28:21 -0300

    Chaging name of the file and making the hook

---
+++ miniblog/src/hooks/useFetchDocuments.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { dataBank } from "../firebase/config";
+import {
+  collection, //Definir a coleção
+  query, //Pegar um dado
+  orderBy, //Ordenção
+  onSnapShot, //Mapear dados que foram alterados
+  where, //Filtro dos resultados que são trazidos
+  QuerySnapshot,
+} from "firebase/firestore";
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+  //Não usei o reducer como no outro hook pois a estrutura é mais simples, apenas estou carregando dados
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  //deal with memory leak
+  const [cancelled, setCancelled] = useState(false);
+
+  useEffect(() => {
+    async function loadData() {
+      if (cancelled) return;
+
+      setLoading(true);
+
+      const collectionRef = await collection(dataBank, docCollection);
+
+      try {
+        let q;
+
+        //busca
+        //dashboard
+
+        q = await query(collectionRef, orderBy("createdAt", "desc")); //Criando busca de forma mais simples. Pegar dados e ordenar pela data decrescente
+        //Mapear os dados. Toda vez que alterar, vai atualizar com os dados novos
+        await onSnapShot(q, (QuerySnapshot) => {
+          setDocuments(
+            QuerySnapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(), //Pegando dados separando em um array. Vindo quase da mesma maneira como foram inseridos
+            }))
+          );
+        });
+
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+        setLoading(false);
+      }
+    }
+    loadData();
+  }, [docCollection, search, uid, cancelled]);
+
+  useEffect(() => {
+    return setCancelled(true);
+  }, []);
+
+  return { documents, loading, error };
+};
