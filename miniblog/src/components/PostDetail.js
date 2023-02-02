19907aaf4de0790ce265ccb804ff4182dcb0c0c3
Yuri <yurifdev@gmail.com>
2023-02-02 17:46:43 -0300

    Setting the class

--- miniblog/src/components/PostDetail.js
+++ miniblog/src/components/PostDetail.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 
 const PostDetail = ({ post }) => {
   return (
-    <div>
+    <div className={styles.post_detail}>
       <img src={post.image} alt={post.title} />
       <h2>{post.title}</h2>
-      <p>{post.createdBy}</p>
+      <p className={styles.createdB}>{post.createdBy}</p>
       <div>
         {post.tagsArray.map((tag) => (
-          <p key={tag}>
+          <p key={tag} className={styles.tags}>
             <span>#</span>
             {tag}
           </p>
