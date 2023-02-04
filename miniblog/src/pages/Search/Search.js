3cf900a3b79f1b6ea455ea6a884f081e24362f25
Yuri <yurifdev@gmail.com>
2023-02-04 14:06:57 -0300

    Setting the search using the hook, and using the components to show the results making a map

--- miniblog/src/pages/Search/Search.js
+++ miniblog/src/pages/Search/Search.js
@@ -1,17 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 //hooks
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useQuery } from "../../hooks/useQuery";
 
+//Components
+import PostDetail from "../../components/PostDetail";
+
+import styles from './Search.module.css'
+
 const Search = () => {
   const query = useQuery();
   const search = query.get("q"); //Vem do URLSearchParams, do hook. Pegando o atributo do 'q', cadastrado lá
 
+  const { documents: posts } = useFetchDocuments("posts", search); //Colection pre definida, na criação dos posts
+
   return (
-    <div>
+    <div className={styles.search_container}>
       <h2>Search</h2>
       <p>{search}</p>
+      <div>
+        {posts && posts.length == 0 && (
+          <div className={styles.noPost}>
+            <p>
+              Não foram encontrados posts a partir da sua busca... tente
+              novamente
+            </p>
+            <Link to="/" className="btn btn-dark">
+              Voltar
+            </Link>
+          </div>
+        )}
+        {posts &&
+          posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
     </div>
   );
 };
