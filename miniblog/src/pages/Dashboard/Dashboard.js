fe735308a66a94e6d045fe5225f7f444d3307cbd
Yuri <yurifdev@gmail.com>
2023-02-06 11:54:16 -0300

    Importing hooks and starting the page's structure

--- miniblog/src/pages/Dashboard/Dashboard.js
+++ miniblog/src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,35 @@
-import React from 'react'
-import styles from "./Dashboard.module.css"
+import React from "react";
+import styles from "./Dashboard.module.css";
+
+import { Link } from "react-router-dom";
+
+//hooks
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
-  return (
-    <div><h2>Dashboard</h2></div>
-  )
-}
+  const { user } = useAuthValue();
+  const uid = user.uid;
 
-export default Dashboard
+  //Post do usuário
+  const posts = [];
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noPosts}>
+          <p>Não foram encontrados posts</p>
+          <Link to='/post/create' className="btn">Criar novo post</Link>
+
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dashboard;
