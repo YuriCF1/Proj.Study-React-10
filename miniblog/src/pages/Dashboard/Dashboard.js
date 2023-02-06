5db5c9648bef357caaa4b7ce0edcf2272dfb1807
Yuri <yurifdev@gmail.com>
2023-02-06 13:07:43 -0300

    Setting the structure of dashboard

--- miniblog/src/pages/Dashboard/Dashboard.js
+++ miniblog/src/pages/Dashboard/Dashboard.js
@@ -12,24 +12,32 @@ const Dashboard = () => {
   const uid = user.uid;
 
   //Post do usuário
-  const posts = [];
+  const {
+    documents: posts,
+    loading,
+    error,
+  } = useFetchDocuments("posts", null, uid);
+
   return (
     <div>
       <h2>Dashboard</h2>
+      <h3>{user.displayName}</h3>
       <p>Gerencie os seus posts</p>
       {posts && posts.length === 0 ? (
         <div className={styles.noPosts}>
           <p>Não foram encontrados posts</p>
-          <Link to='/post/create' className="btn">Criar novo post</Link>
-
+          <Link to="/post/create" className="btn">
+            Criar novo post
+          </Link>
         </div>
       ) : (
         <div>
           <p>Tem posts</p>
         </div>
       )}
+      {posts && posts.map((post) => <h3>{post.title}</h3>)}
     </div>
-  );
+  ); 
 };
 
 export default Dashboard;
