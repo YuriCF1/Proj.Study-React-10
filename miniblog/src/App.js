9383360e95ffe81928c6bf93ea3824871843f090
Yuri <yurifdev@gmail.com>
2023-02-09 22:47:18 -0300

    Setting routes to change the post

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -20,6 +20,7 @@ import CreatePost from "./pages/CreatePost/CreatePost";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Search from "./pages/Search/Search";
 import Post from "./pages/Post/Post";
+import EditPost from "./pages/EditPost/EditPost";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -49,6 +50,10 @@ function App() {
               <Route path="/search" element={<Search />}></Route>
               <Route path="/post/:id" element={<Post />}></Route>
               <Route
+                path="/post/edit/:id"
+                element={user ? <EditPost /> : <Navigate to={"/login"} />}
+              />
+              <Route
                 path="/login"
                 element={!user ? <Login /> : <Navigate to={"/"} />} //*Reirecionando clicks de acordo com status do usuário
               />
@@ -60,6 +65,7 @@ function App() {
                 path="/post/create"
                 element={user ? <CreatePost /> : <Navigate to={"/login"} />}
               />
+
               <Route
                 path="/dashboard"
                 element={user ? <Dashboard /> : <Navigate to={"/login"} />}
